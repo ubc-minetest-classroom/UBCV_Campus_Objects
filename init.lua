8acfdb1fcdb8d45ae8d2b0ad2d21b93a80bebac7
pauldpickell <61996662+pauldpickell@users.noreply.github.com>
2022-08-19 14:42:35 -0700

    fix indents and pole v1

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local node_def = {
     drawtype = "mesh",
     mesh = "ubc_sign_final.obj",
     tiles = {"ubc_sign_texture.png"},
-	groups = {oddly_breakable_by_hand=3}
+    groups = {oddly_breakable_by_hand=3}
 }
 minetest.register_node("ubcv_campus_objects:ubc_sign",node_def)
 
@@ -12,7 +12,7 @@ local node_def = {
     drawtype = "mesh",
     mesh = "totem_final_3.obj",
     tiles = {"totem_pole_texture.png"},
-	groups = {oddly_breakable_by_hand=3}
+    groups = {oddly_breakable_by_hand=3}
 }
 minetest.register_node("ubcv_campus_objects:musqueam_post",node_def)
 
@@ -22,7 +22,7 @@ local node_def = {
     drawtype = "mesh",
     mesh = "engineer_block.obj",
     tiles = {"e-white.png", "e-red.png"},
-	groups = {oddly_breakable_by_hand=3}
+    groups = {oddly_breakable_by_hand=3}
 }
 minetest.register_node("ubcv_campus_objects:engineering_cairn",node_def)
 
@@ -31,24 +31,15 @@ local node_def = {
     drawtype = "mesh",
     mesh = "icisis_model.obj",
     tiles = {"black.png", "blue.png", "green.png", "red.png"},
-	groups = {oddly_breakable_by_hand=3}
+    groups = {oddly_breakable_by_hand=3}
 }
 minetest.register_node("ubcv_campus_objects:icisis",node_def)
 
 local node_def = {
     sunlight_propagates = false,
     drawtype = "mesh",
-    mesh = "banner_pole.obj",
+    mesh = "banner_v1.obj",
     tiles = {"pole_base.png", "pole_light.png", "banner.png"},
-	groups = {oddly_breakable_by_hand=3}
+    groups = {oddly_breakable_by_hand=3}
 }
-minetest.register_node("ubcv_campus_objects:light_pole_banner",node_def)
-
-local node_def = {
-    sunlight_propagates = false,
-    drawtype = "mesh",
-    mesh = "light_pole.obj",
-    tiles = {"pole_base.png", "pole_light.png"},
-	groups = {oddly_breakable_by_hand=3}
-}
-minetest.register_node("ubcv_campus_objects:light_pole",node_def)
+minetest.register_node("ubcv_campus_objects:light_pole_1",node_def)
